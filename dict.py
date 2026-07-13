c8b5891a8d92ca445457c27290c1a201dee47255
nutissck <valeradoyko@gmail.com>
2026-07-13 14:26:01 +0300

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,41 @@
+#dict словник не індексований тип даних
+#структура даних яка зберігає пари
+#ключ: значення
+"""a={
+    "name":"Tom",
+    "age":25,
+    "city":"London"
+}
+d1={
+    "a":1,
+    "b":2,
+    "c":3,
+}"""
+"""a={
+    "name":"Tom",
+    "age":25,
+    "city":"London"
+}
+print(a["name"])
+print(a["age"])
+a["name"]="Bob"
+print(a["name"])"""
+#-------------------------------
+#методи словників
+a={
+    "name":"Tom",
+    "age":25,
+    "city":"London"
+}
+print(a.keys())
+print(a.values())
+print(a.items())
+
+
+
+
+
+
+
+
+
---
+++ string.py
@@ -0,0 +1,14 @@
+#str - послідовність символів
+#індексований тип даних
+#незмінюваний
+"""text="Python"
+print(text[0])
+print(text[-1])
+print(text[::2])
+print(text[3:])
+print(text[:6])"""
+text="Python"
+L=list(text)
+print(L)
+r=str(text)
+print(r)
